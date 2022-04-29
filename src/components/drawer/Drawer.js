d08f6c158b8ba756b71c5b315cd5d4e962ec1575
azher ali <azhermurad@gmial.com>
2022-04-29 16:39:23 +0500

    drawer

--- src/components/drawer/Drawer.js
+++ src/components/drawer/Drawer.js
@@ -1,69 +1,60 @@
-// import React from "react";
-// import clsx from "clsx";
-// import { makeStyles } from "@material-ui/core/styles";
-// import Drawer from "@material-ui/core/Drawer";
-// import Button from "@material-ui/core/Button";
-// import List from "@material-ui/core/List";
-// import Divider from "@material-ui/core/Divider";
-// import ListItem from "@material-ui/core/ListItem";
-// import ListItemIcon from "@material-ui/core/ListItemIcon";
-// import ListItemText from "@material-ui/core/ListItemText";
-// import InboxIcon from "@material-ui/icons/MoveToInbox";
-// import MailIcon from "@material-ui/icons/Mail";
+import React from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import MailIcon from "@material-ui/icons/Mail";
 
-// const useStyles = makeStyles({
-//   list: {
-//     width: 250,
-//   },
-//   fullList: {
-//     width: "auto",
-//   },
-// });
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: "auto",
+  },
+});
 
-// export default function TemporaryDrawer() {
-//   const classes = useStyles();
-//   const [state, setState] = React.useState({
-//     top: false,
-//     left: false,
-//     bottom: false,
-//     right: false,
-//   });
+export default function TemporaryDrawer({toggleDrawer,isOpen}) {
+  const classes = useStyles();
+  
 
-//   const toggleDrawer = (anchor, open) => (event) => {};
+  const list = (anchor) => (
+    <div
+      className={classes.list}
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {["Home", "Reward", "Swap"].map((text, index) => (
+          <ListItem button key={text} onClick={() => {}}>
+            <ListItemIcon>
+              <MailIcon />
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </div>
+  );
 
-//   const list = (anchor) => (
-//     <div
-//       className={classes.list}
-//       role="presentation"
-//       //   onClick={toggleDrawer(anchor, false)}
-//       //   onKeyDown={toggleDrawer(anchor, false)}
-//     >
-//       <List>
-//         {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-//           <ListItem button key={text}>
-//             <ListItemIcon>
-//               {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-//             </ListItemIcon>
-//             <ListItemText primary={text} />
-//           </ListItem>
-//         ))}
-//       </List>
-//       <Divider />
-//     </div>
-//   );
-
-//   return (
-//     <div>
-//       <React.Fragment>
-//         <Button onClick={toggleDrawer("left", true)}>btssn</Button>
-//         <Drawer
-//           anchor={"left"}
-//           open={state["left"]}
-//           onClose={toggleDrawer("left", false)}
-//         >
-//           {list("left")}
-//         </Drawer>
-//       </React.Fragment>
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <React.Fragment>
+        <Button onClick={toggleDrawer("left", true)}>left</Button>
+        <Drawer
+          anchor={"left"}
+          open={isOpen}
+          onClose={toggleDrawer("left", false)}
+        >
+          {list("left")}
+        </Drawer>
+      </React.Fragment>
+    </div>
+  );
+}

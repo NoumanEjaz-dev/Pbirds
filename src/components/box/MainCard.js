f7572cabaeb38d6d1f6b542faf3e80af20ef0253
NoumanEjaz-dev <NomeejaniJj43@gmail.com>
2022-04-30 07:34:58 -0700

    new changes added in the pbirds project

---
+++ src/components/box/MainCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { Box, makeStyles, Paper, Card } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: theme.palette.common.lightDark,
+    borderRadius: "5px",
+  },
+}));
+
+const MainCard = ({ children }) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>{children}</CardContent>
+    </Card>
+  );
+};
+
+export default MainCard;
--- src/pages/Reward.js
+++ src/pages/Reward.js
@@ -1,7 +1,132 @@
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import { Box, Button } from "@material-ui/core";
+import ChartGraph from "../components/charts/ChartGraph";
+import CardBox from "../components/card/CardBox";
+import MainCard from "../components/box/MainCard";
+import CommunityImage from "../assest/images/JoinOurCommunity.png";
+import profile from "../assest/images/profile.png";
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+      paddingTop: "20px",
+      textAlign: "center",
+    },
+    paper: {
+      padding: theme.spacing(2),
+    },
+    JoinCommunityTitle: {
+      color: theme.palette.common.white,
+      fontFamily: "Roboto",
+    },
+    JoinCommunityText: {
+      fontFamily: "Roboto",
+      fontSize: "12px",
+      color: theme.palette.common.white,
+      // margin: "9px",
+    },
+    JoinButton: {
+      backgroundColor: "#2B2B2B",
+      borderRadius: "10px",
+      color: theme.palette.common.white,
+      marginTop: "10px",
+      width: "100%",
+    },
+    RewardCard: {
+      display: "flex",
+      justifyContent: "space-between",
+      textAlign: "center",
+    },
+    Title: {
+      display: "flex",
+      alignItems: "center",
+      color: theme.palette.common.white,
+      fontFamily: "Roboto",
+    },
+    RewardClaimed: {
+      color: theme.palette.common.white,
+      fontFamily: "Roboto",
+    },
+    Na: {
+      margin: "10px",
+      color: theme.palette.common.white,
+      fontFamily: "Roboto",
+    },
+    ClaimBtn: {
+      margin: "10px",
+      color: theme.palette.common.white,
+      fontFamily: "Roboto",
+      backgroundColor: "#D09A0A",
+      padding: "10px",
+      "&:hover": {
+        backgroundColor: "#D09A0A",
+      },
+    },
+    TotalEarned: {
+      margin: "10px",
+      color: theme.palette.common.white,
+      fontFamily: "Roboto",
+    },
+    TotalDistributed: {
+      margin: "10px",
+      color: theme.palette.common.white,
+      fontFamily: "Roboto",
+    },
+  })
+);
+
 const RewardPage = () => {
+  const classes = useStyles();
+
   return (
     <>
-      <h1>RewardPage</h1>
+      <div className={classes.root}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} lg={6} spacing={3}>
+            <ChartGraph />
+          </Grid>
+          <Grid item xs={6} lg={3} spacing={3}>
+            <CardBox />
+          </Grid>
+          <Grid item xs={6} lg={3} spacing={3}>
+            <MainCard>
+              <img
+                className={classes.JoinImage}
+                src={CommunityImage}
+                alt="img"
+              />
+              <p className={classes.JoinCommunityTitle}>Join Our Community</p>
+              <p className={classes.JoinCommunityText}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+              <Button className={classes.JoinButton}>Join Now</Button>
+            </MainCard>
+          </Grid>
+        </Grid>
+        {/*  */}
+        <Grid container spacing={2}>
+          <Grid item xs={6} lg={3} spacing={3}>
+            <MainCard>
+              <div>
+                <div className={classes.RewardCard}>
+                  <img src={profile} alt="img" />
+                  <h3 className={classes.Title}>WBNB</h3>
+                </div>
+                <hr />
+                <p className={classes.RewardClaimed}>Reward Not Claimed</p>
+                <p className={classes.Na}>N/A</p>
+                <Button className={classes.ClaimBtn}>Claim Manually</Button>
+                <p className={classes.TotalEarned}>Total Earned (WBNB)</p>
+                <p className={classes.Na}>N/A</p>
+                <p className={classes.TotalDistributed}>
+                  Total (WBNB) Distributed
+                </p>
+              </div>
+            </MainCard>
+          </Grid>
+        </Grid>
+      </div>
     </>
   );
 };

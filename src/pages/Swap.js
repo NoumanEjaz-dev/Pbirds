20cd1aa3c257b7513df9a53a1a97fbc1bf471702
azher ali <azhermurad@gmial.com>
2022-05-05 21:39:44 +0500

    tabs

--- src/pages/Swap.js
+++ src/pages/Swap.js
@@ -1,12 +1,10 @@
 import SwapForm from "../components/swapform/SwapForm";
 import TransferForm from "../components/transferFom/TransferForm";
-
+import SimpleTabs from "../components/formtabs"
 const SwapPage = () => {
   return (
     <>
-      <SwapForm />
-      {/*  */}
-      <TransferForm />
+ <SimpleTabs/>   
     </>
   );
 };
